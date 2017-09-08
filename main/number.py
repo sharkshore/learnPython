37dc5189f860808b5e0fb396e4d06a69542836f2
shark <sharkshore@163.com>
2017-09-08 17:36:45 +0800

    添加number.py

---
+++ main/number.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'BF100395'
+__mtime__ = '2017/9/8'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+
+#数字类型转换
+from random import uniform
+
+from math import sin, pi, e
+
+"""
+a=1.0
+b=int(a)
+c=float(b)
+d=complex(a)
+print(a)
+print(b)
+print(c)
+print(d)
+
+"""
+
+print(3/2)
+print(3//2)
+print(uniform(1,2))
+
+rad=(30/360)*2*pi
+s=sin(rad)
+print(s)
+
+print(e**e)
