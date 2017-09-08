3cffebd56fc960b049fd18fea703cc4ed1536c61
shark <sharkshore@163.com>
2017-09-08 10:52:29 +0800

    first commit

---
+++ main/data_structure.py
@@ -0,0 +1,16 @@
+#python3数据结构
+
+#列表当做堆栈
+'''
+stack=[3,4,5]
+stack.append(6)
+stack.append(7)
+print("第一次打印",stack)
+stack.pop()
+print("第一次弹出后",stack)
+stack.pop()
+print("第二次弹出后",stack)
+'''
+
+
+#列表当做
