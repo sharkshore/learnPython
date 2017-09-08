cefae40073d1470581a250ce1893d6ac5494e856
shark <sharkshore@163.com>
2017-09-08 20:26:43 +0800

    yuanzu.py

---
+++ main/tup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'BF100395'
+__mtime__ = '2017/9/8'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+
+
+#创建元组
+"""
+
+tup1 = ('Google', 'Runoob', 1997, 2000);
+tup2 = (1, 2, 3, 4, 5 );
+tup3 = "a", "b", "c", "d";
+print('tup1[0]',tup1[0])
+print('tup2[0]',tup2[0])
+print('tup3[0]',tup3[0])
+print(tup3[1:5])
+"""
+
